0712c7272dcba7155ecdab01c9ccfe134dece960
htsang1904 <111369238+htsang1904@users.noreply.github.com>
2022-12-19 13:31:58 +0700

    sang

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const bar = document.getElementById('bar');
 const navbar = document.getElementById('navbar');
+const close = document.getElementById('close');
 if(bar) {
   bar.addEventListener('click', () => {
     navbar.classList.add('active');
+  })
+}
+if(close) {
+  close.addEventListener('click', () => {
+    navbar.classList.remove('active');
   })
 }
